65f8a59a1b5d364714ed72f283f2e8d7f255c9b0
dimasdevspro <dimasdevspro@github.com>
2020-10-05 18:34:55 -0300

    update server; create  views; install dependences  nodemon and nunjucks.

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const server = express()
+const nunjucks = require('nunjucks')
+
+server.get("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+server.get("/", function(req, res){
+    return res.send("Hi! How's going? Many work?")
+})
 
 server.listen(5000, function(){
     console.log("Server is running!")
 })
+
