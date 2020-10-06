8b2c30efc2642925ff7a4a8d221e51305bb8ea08
dimasdevspro <dimasdevspro@github.com>
2020-10-06 13:20:07 -0300

    update *.html; add public, views and server.js

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express')
-const server = express()
 const nunjucks = require('nunjucks')
 
-server.get("view engine", "html")
+const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "html")
 
 nunjucks.configure("views", {
     express: server
 })
+
 server.get("/", function(req, res){
-    return res.send("Hi! How's going? Many work?")
+    return res.render("index")
 })
 
-server.listen(5000, function(){
+server.get("/views/about.html", function(req, res){
+    return res.render("about")
+})
+
+server.get("/views/container.html", function(req, res){
+    return res.render("container")
+})
+
+server.listen(3000, function(){
     console.log("Server is running!")
 })
 
