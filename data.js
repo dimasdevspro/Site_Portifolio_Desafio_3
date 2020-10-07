c3b9e423f7f7bb1fd04b81fc2a23a347ac545ea1
dimasdevspro <dimasdevspro@github.com>
2020-10-07 17:44:34 -0300

    building backend

---
+++ data.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id:"electron-construindo-aplicacoes-desktop-com-javascript/",
+        img_url:"https://blog.rocketseat.com.br/content/images/2020/08/blog-rocketseat-construindo-apps-desktop-com-electron-1.png",
+        title_card_content: "Electron - construindo aplicações desktop com JavaScript",
+        author_card_info:"Thiago Marinho",
+        time_card_timetoread:"10 min de Leitura"
+
+    },
+    {
+        id:"tipos-de-parametros-nas-requisicoes-rest/",
+        img_url:"https://blog.rocketseat.com.br/content/images/2020/08/tipos-de-parametros-nas-requisicoes-rest.png",
+        title_card_content: "Tipos de Parâmetros nas requisições REST",
+        author_card_info:"Thiago Martins",
+        time_card_timetoread:"02 min de Leitura"
+
+    },
+    {
+        id:"introducao-ao-docker-criando-um-servidor-web-com-node-js-e-subindo-para-o-container/",
+        img_url:"https://blog.rocketseat.com.br/content/images/2020/08/introducao-ao-docker.png",
+        title_card_content: "Introdução ao Docker - Criando um servidor web com Node.js e subindo para o container",
+        author_card_info:"Thiago Martins",
+        time_card_timetoread:"09 min de Leitura"
+
+    },
+]
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const blogs = require("./data")
+
 
 server.use(express.static('public'))
 
@@ -12,18 +14,37 @@ nunjucks.configure("views", {
 })
 
 server.get("/", function(req, res){
-    return res.render("about")
+    const about = {
+        avatar_url:"https://avatars1.githubusercontent.com/u/28929274?s=280&v=4",
+        title_info:"Descrição da empresa",
+        description_title:"Mais do que uma plataforma de educação em tecnologia, somos uma comunidade incrível de programadores em busca do próximo nível",
+        title_tech:"Principais tecnologias utilizadas",
+        techs:[
+            { name: "CSS"},
+            { name: "HTML"}, 
+            { name: "JavaScript"},
+            { name: "Node JS"},
+            { name: "React JS"},
+            { name: "React Native"}, 
+        ],
+        links:[
+            { name:"Github", url:"https://github.com/rocketseat-education/"},
+            { name:"Instagram", url:"https://www.instagram.com/rocketseat_oficial/?hl=en"},
+            { name:"Facebook", url:"https://pt-br.facebook.com/rocketseat/"},            
+        ],
+    }
+
+        
+    return res.render("about", { about })
 })
 
-server.get("/views/about.njk", function(req, res){
-    return res.render("about")
+server.get("/container.njk", function(req, res){
+
+
+    return res.render("container", {items: blogs})
 })
 
-server.get("/views/container.njk", function(req, res){
-    return res.render("container")
-})
-
-server.get("/views/layout.njk", function(req, res){
+server.get("/layout.njk", function(req, res){
     return res.render("layout")
 })
 
